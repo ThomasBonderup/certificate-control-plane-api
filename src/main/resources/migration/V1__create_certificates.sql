a6a58fb21b4d35feda2c7544e1fa19f94682b8ff
Thomas Winther Bonderup <tb@tbcoding.dk>
2026-04-14 12:10:21 +0200

    setup Application config for postgres

--- src/main/resources/migration/V1__create_certificates.sql
+++ src/main/resources/migration/V1__create_certificates.sql
@@ -1,4 +1,4 @@
-create table certificates {
+create table certificates (
   id uuid primary key,
   tenant_id varchar(100) not null,
   name varchar(255) not null,
@@ -6,12 +6,12 @@ create table certificates {
   issuer varchar(255),
   serial_number varchar(255),
   sha256_fingerprint varchar(255),
-  not_before timestamptz,
-  not_after timestamptz,
+  not_before timestamp with time zone,
+  not_after timestamp with time zone,
   status varchar(50) not null,
-  renewal_status(50) not null,
+  renewal_status varchar(50) not null,
   owner varchar(255),
   notes text,
-  created_at timestamptz not null,
-  updated_at timestamptz not null
-};
+  created_at timestamp with time zone not null,
+  updated_at timestamp with time zone not null
+);
