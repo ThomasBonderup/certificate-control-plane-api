fd0f521cdc1bfc25f0e72f7afad5b34597b8a91f
Thomas Winther Bonderup <tb@tbcoding.dk>
2026-04-14 12:10:21 +0200

    setup Application config for postgres

--- src/main/java/com/combotto/controlplane/services/CertificateService.java
+++ src/main/java/com/combotto/controlplane/services/CertificateService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
-import java.util.List
+import java.util.List;
 
 @Service
 public class CertificateService {
--- src/test/java/com/combotto/controlplane/KafkaProducerConfigTest.java
+++ src/test/java/com/combotto/controlplane/KafkaProducerConfigTest.java
@@ -10,12 +10,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = "spring.kafka.bootstrap-servers=test-broker:19092")
+import com.combotto.controlplane.repositories.CertificateRepository;
+
+@SpringBootTest(properties = {
+    "spring.kafka.bootstrap-servers=test-broker:19092",
+    "spring.flyway.enabled=false",
+    "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,"
+        + "org.springframework.boot.jdbc.autoconfigure.DataSourceInitializationAutoConfiguration,"
+        + "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration,"
+        + "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration"
+})
 class KafkaProducerConfigTest {
 
   @Autowired
   ProducerFactory<?, ?> producerFactory;
+
+  @MockitoBean
+  CertificateRepository certificateRepository;
 
   @Test
   void producerFactoryUsesConfiguredBootstrapServers() {
